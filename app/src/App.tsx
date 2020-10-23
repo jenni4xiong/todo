b56df8ad3d3161e7b5fa27f96d7643a8d0825b28
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-22 21:26:01 -0700

    add todo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,30 @@
 import React, { Fragment, useState } from 'react';
 import './App.css';
 
+//variables for types that already exists
 type FormElem = React.FormEvent<HTMLFormElement>
 type ChangeElem = React.ChangeEvent<HTMLInputElement>
 
+//interface creates new type
+interface ITodo {
+  text: string,
+  complete: boolean
+}
+
+//you can extend interfaces and you can't extend types (similar to extending class)
+// interface ITodo2 extends ITodo{
+//   tag: string[]
+// }
+
+
 const App = (): JSX.Element => {
   const [value, setValue] = useState<string>('')
+  //need to put the [] after ITodo because it needs to show that it is an array of ITodo objects
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   const handleSubmit = (e: FormElem): void => {
     e.preventDefault()
+    addTodo(value)
     setValue('')
   }
 
@@ -16,6 +32,13 @@ const App = (): JSX.Element => {
     setValue(e.target.value)
   }
 
+  const addTodo = (text: string): void => {
+    //add new todoes to the copy of the current todos state
+    const newTodos: ITodo[] = [...todos, { text, complete: false }]
+    //set newTodos
+    setTodos(newTodos)
+  }
+  console.log('todos', todos)
   return (
     <Fragment>
       <h1>To Do List</h1>
